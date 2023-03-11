f5dee398f6fac4888904948fac0ee7abed5940f0
biroue isaac <biroueisaac@gmail.com>
2023-03-11 21:55:17 +0100

    Initial commit

---
+++ listings/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.TextField()
+    #image = models.ImageField()
+
+
